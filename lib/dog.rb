ae237e3e8a7c99d29722c560e37741c740814365
BethAnn Hanlon <herbivore137@gmail.com>
2019-05-29 03:12:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,7 +40,8 @@ class Dog
   
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
-    result = 
+    result = DB[:conn].execute(sql, name)[0]
+    Dog.new({id: result[0], name: result[1], breed: result[2]})
   end
   
   def self.find_or_create_by(name:, breed:)
